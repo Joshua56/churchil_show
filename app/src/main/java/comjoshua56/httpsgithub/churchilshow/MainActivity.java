645724e28f54d9242a91253e61b909e20990b38c
Joshua Chirchir <https://github.com/Joshua56>
2017-10-04 21:31:02 +0300

    First commit

---
+++ app/src/main/java/comjoshua56/httpsgithub/churchilshow/MainActivity.java
@@ -0,0 +1,108 @@
+package comjoshua56.httpsgithub.churchilshow;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager mPager;
+    private MyPagerAdapter mAdapters;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout mTablayout = (TabLayout) findViewById(R.id.tab_layout);
+        mPager=(ViewPager) findViewById(R.id.pager);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class MyFragment extends Fragment{
+
+        public MyFragment(){
+
+
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+            TextView myText = new TextView(getActivity());
+            myText.setText("Hello i am here");
+            myText.setGravity(Gravity.CENTER);
+            return myText;
+        }
+    }
+
+ private class MyPagerAdapter extends FragmentPagerAdapter {
+
+
+     public MyPagerAdapter(FragmentManager fm) {
+         super(fm);
+     }
+
+     @Override
+        public Fragment getItem(int position) {
+         MainActivityMyFragment
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+     @Override
+     public CharSequence getPageTitle(int position) {
+         return super.getPageTitle(position);
+     }
+ }
+}
